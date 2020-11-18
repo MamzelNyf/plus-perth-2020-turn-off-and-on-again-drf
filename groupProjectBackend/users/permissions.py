caf64c5912f18d845a5271cc7fe67dc630777358
MamzelNyf <fanny@shedesign.dev>
2020-11-18 17:01:46 +0800

    update permission

--- groupProjectBackend/users/permissions.py
+++ groupProjectBackend/users/permissions.py
@@ -5,6 +5,6 @@ class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(Self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.username == request.user
+        return obj.username == request.user.username
         
 
